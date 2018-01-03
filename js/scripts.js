0b2e0d239038a5043ff7b97d4be8de604ec0e316
Paweł Galeja <pgaleja@gmail.com>
2018-01-03 21:21:20 +0100

    Add iterating through elements in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur = 'triceratops';
-var dinosaurUpperCased = dinosaur.toUpperCase();
-var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
-console.log(textCharsAfter.substr(0, textCharsAfter.length / 2));
+var buttons = document.getElementsByClassName("button");
+
+for (var i = 0; i < buttons.length; i++) {
+    alert(buttons[i].innerHTML);
+}
